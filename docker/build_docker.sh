f748c6ad6ef794fe0694c8aa8c3fece8c97db42f
Mattias Vendler <mattias.vendler@gmail.com>
2021-04-25 20:55:22 +0200

    Fix docker build script

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -6,7 +6,7 @@ rm conbeeii-service-0.0.1-SNAPSHOT.jar
 cp ../target/conbeeii-service-0.0.1-SNAPSHOT.jar .
 export DOCKER_HOST=192.168.1.181
 docker build -t conbeeii-service .
-docker stop conbeeII-service
-docker rm ha-service
+docker stop conbeeii-service
+docker rm conbeeii-service
 docker run -d --link test-mysql --name conbeeii-service conbeeii-service:latest
 docker logs -f conbeeii-service | less
